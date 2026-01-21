522e01f00cce0cfd95dca0fe40735102b2be105d
new commit <ebhos4real@yahoo.com>
2026-01-21 00:11:20 -0500

    created frontend Script expenconf sample script for demo added to mysql

--- frontend.sh
+++ frontend.sh
@@ -6,5 +6,4 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-## copy file step
 systemctl restart nginx
