888bbd4bd935603d616e7dd1dadae8ca8e5bcccd
new commit <ebhos4real@yahoo.com>
2026-01-22 18:20:53 -0500

    created frontend Script added edited backend.sh echo print
    expenconf
    backend script for demo
    added to mysql

--- frontend.sh
+++ frontend.sh
@@ -9,7 +9,7 @@ rm -rf /usr/share/nginx/html/*
 
 echo -e "\e[3535mcleandownload frontend content\e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
-cd  /usr/share/nginx/html
+cd /usr/share/nginx/html
 
 echo -e "\e[35mextract content\e[0m"
 unzip /tmp/frontend.zip
