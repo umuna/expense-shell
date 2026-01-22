28e521e3ae927eb041f63255a168b16ade32c493
new commit <ebhos4real@yahoo.com>
2026-01-22 17:01:26 -0500

    created frontend Script added edited backend.sh echo print
    expenconf
    backend script for demo
    added to mysql

--- frontend.sh
+++ frontend.sh
@@ -1,24 +1,20 @@
-echo -e "\e[33mInstalling Nginx\e[0m"
+echo -e "\e[35mInstalling Nginx\e[0m"
 dnf install nginx -y
 
-echo -e "\e[33mCopy expense  config file
+echo -e "\e[35mCopy expense  config file"
 cp expense.conf /etc/nginx/default.d/expense.conf
 
-echo -e \e[33mclean old content
+echo -e "\e[35mclean old content\e[0m"
 rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[33m]download frontend content
+echo -e "\e[35download frontend content\e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
 
-echo -e \e[33extract content\e[0m"
+echo -e \e[35mextract content\e[0m"
 unzip /tmp/frontend.zip
 
-echo -e "\ee[33restart service
+echo -e "\e[35mextract service\e[0m"
 systemctl restart service
 systemctl enable nginx
 
-
-
-unzip /tmp/frontend.zip
-systemctl restart nginx
